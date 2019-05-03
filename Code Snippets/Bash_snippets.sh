e0fad1bdd6493a97fa08b41fd0eeb208714e2b5e
giladamar <giladamar@gmail.com>
2019-05-03 13:00:51 +0200

    bash_snippets:     clean up st and stderr output to file

--- Code Snippets/Bash_snippets.sh
+++ Code Snippets/Bash_snippets.sh
@@ -51,90 +51,27 @@ Splitting up mp3 files:
 
 
 #Restarting bash command on failure:
-until !!; do echo Command interrupted, restarting in 5s ...;sleep 5;done
+until !!; do echo Command interrupted, restarting in 5s ...; sleep 5; done
 
-#TODO
+
 Save bash output to file:
-    Yes it is possible, just redirect the output to a file:
-
+e.g.
     SomeCommand > SomeFile.txt
-    Or if you want to append data:
 
-    SomeCommand >> SomeFile.txt
-    If you want stderr as well use this:
-
-    SomeCommand &> SomeFile.txt
-    or this to append:
-
-    SomeCommand &>> SomeFile.txt
-    if you want to have both stderr and output displayed on the console and in a file use this:
-
-    SomeCommand 2>&1 | tee SomeFile.txt
-
-    Please note that the n.e. in the syntax column means "not existing".
-There is a way, but it's too complicated to fit into the column. You can find a helpful link in the List section about it.
-
-          || visible in terminal ||   visible in file   || existing
-  Syntax  ||  StdOut  |  StdErr  ||  StdOut  |  StdErr  ||   file
-==========++==========+==========++==========+==========++===========
-    >     ||    no    |   yes    ||   yes    |    no    || overwrite
-    >>    ||    no    |   yes    ||   yes    |    no    ||  append
-          ||          |          ||          |          ||
-   2>     ||   yes    |    no    ||    no    |   yes    || overwrite
-   2>>    ||   yes    |    no    ||    no    |   yes    ||  append
-          ||          |          ||          |          ||
-   &>     ||    no    |    no    ||   yes    |   yes    || overwrite
-   &>>    ||    no    |    no    ||   yes    |   yes    ||  append
-          ||          |          ||          |          ||
- | tee    ||   yes    |   yes    ||   yes    |    no    || overwrite
- | tee -a ||   yes    |   yes    ||   yes    |    no    ||  append
-          ||          |          ||          |          ||
- n.e. (*) ||   yes    |   yes    ||    no    |   yes    || overwrite
- n.e. (*) ||   yes    |   yes    ||    no    |   yes    ||  append
-          ||          |          ||          |          ||
-|& tee    ||   yes    |   yes    ||   yes    |   yes    || overwrite
-|& tee -a ||   yes    |   yes    ||   yes    |   yes    ||  append
-List:
-command > output.txt
-
-The standard output stream will be redirected to the file only, it will not be visible in the terminal. If the file already exists, it gets overwritten.
-
-command >> output.txt
-
-The standard output stream will be redirected to the file only, it will not be visible in the terminal. If the file already exists, the new data will get appended to the end of the file.
-
-command 2> output.txt
-
-The standard error stream will be redirected to the file only, it will not be visible in the terminal. If the file already exists, it gets overwritten.
-
-command 2>> output.txt
-
-The standard error stream will be redirected to the file only, it will not be visible in the terminal. If the file already exists, the new data will get appended to the end of the file.
-
-command &> output.txt
-
-Both the standard output and standard error stream will be redirected to the file only, nothing will be visible in the terminal. If the file already exists, it gets overwritten.
-
-command &>> output.txt
-
-Both the standard output and standard error stream will be redirected to the file only, nothing will be visible in the terminal. If the file already exists, the new data will get appended to the end of the file..
-
-command | tee output.txt
-
-The standard output stream will be copied to the file, it will still be visible in the terminal. If the file already exists, it gets overwritten.
-
-command | tee -a output.txt
-
-The standard output stream will be copied to the file, it will still be visible in the terminal. If the file already exists, the new data will get appended to the end of the file.
-
-(*)
-
-Bash has no shorthand syntax that allows piping only StdErr to a second command, which would be needed here in combination with tee again to complete the table. If you really need something like that, please look at "How to pipe stderr, and not stdout?" on Stack Overflow for some ways how this can be done e.g. by swapping streams or using process substitution.
-
-command |& tee output.txt
-
-Both the standard output and standard error streams will be copied to the file while still being visible in the terminal. If the file already exists, it gets overwritten.
-
-command |& tee -a output.txt
-
-Both the standard output and standard error streams will be copied to the file while still being visible in the terminal. If the file already exists, the new data will get appended to the end of the file.
+    ||          || visible in terminal ||   visible in file   || existing
+    ||  Syntax  ||  StdOut  |  StdErr  ||  StdOut  |  StdErr  ||   file
+    ||==========++==========+==========++==========+==========++===========
+    ||    >     ||    no    |   yes    ||   yes    |    no    || overwrite
+    ||    >>    ||    no    |   yes    ||   yes    |    no    ||  append
+    ||          ||          |          ||          |          ||
+    ||   2>     ||   yes    |    no    ||    no    |   yes    || overwrite
+    ||   2>>    ||   yes    |    no    ||    no    |   yes    ||  append
+    ||          ||          |          ||          |          ||
+    ||   &>     ||    no    |    no    ||   yes    |   yes    || overwrite
+    ||   &>>    ||    no    |    no    ||   yes    |   yes    ||  append
+    ||          ||          |          ||          |          ||
+    || | tee    ||   yes    |   yes    ||   yes    |    no    || overwrite
+    || | tee -a ||   yes    |   yes    ||   yes    |    no    ||  append
+    ||          ||          |          ||          |          ||
+    || |& tee   ||   yes    |   yes    ||   yes    |   yes    || overwrite
+    || |& tee -a||   yes    |   yes    ||   yes    |   yes    ||  append
